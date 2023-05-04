2525e07836b439e6f81c83815f4358ea05319422
yuwei wang <w67238@outlook.com>
2023-05-04 15:58:48 +1200

    main page layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>PEPEHA</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <img src="/public/mt1.png" alt="pepeha img" width="500" height="500"/>
+        {/* link to pepeha intro */}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <button>CREATE PEPEHA</button>
+        {/* link to pepeha creation */}
+      </div>
+
+
+      
     </>
   )
 }
