49ee09c5b74e49701794135203998e9ebd8e6511
yuwei wang <w67238@outlook.com>
2023-05-05 18:58:02 +1200

    my pepeha post endpoint setup

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import cors from "cors"
 
-const app = express()
+const app = express();
 
+app.use(express.json())
 app.use(cors())
 
 const port = process.env.PORT || 3005
@@ -10,6 +11,12 @@ const port = process.env.PORT || 3005
 // endpoint
 app.get("/", (req,res) => {
   res.send("message from server index.js")
+})
+
+app.post("/myPepeha", (req, res) => {
+  const myPepehaInfo = req.body.pepehaDescription
+  console.log("your pepeha info: ", myPepehaInfo)
+  res.json({pepehaDescription: `you wrote: ${myPepehaInfo}`})
 })
 
 app.listen(port, ()=> {
